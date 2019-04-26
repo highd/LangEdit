3d54d39bb37e1126e212e1dde3678a63236cd6d9
hdgames123 <hdgames123@yahoo.co.jp>
2019-04-27 01:05:06 +0900

    初期レイアウト

---
+++ LangEditor/LangData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace LangEditor {
+    public class LangData : INotifyPropertyChanged {
+        private bool _IsExpanded = true;
+        private string _Text = "";
+        private LangData _Parent = null;
+        private ObservableCollection<LangData> _Children = null;
+
+
+        public bool IsExpanded {
+            get { return _IsExpanded; }
+            set { _IsExpanded = value; OnPropertyChanged("IsExpanded"); }
+        }
+
+        public string Text {
+            get { return _Text; }
+            set { _Text = value; OnPropertyChanged("Text"); }
+        }
+
+        public LangData Parent {
+            get { return _Parent; }
+            set { _Parent = value; OnPropertyChanged("Parent"); }
+        }
+
+        public ObservableCollection<LangData> Children {
+            get { return _Children; }
+            set { _Children = value; OnPropertyChanged("Childen"); }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name) {
+            if (null == this.PropertyChanged) return;
+            this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void Add(LangData child) {
+            if (null == Children) Children = new ObservableCollection<LangData>();
+            child.Parent = this;
+            Children.Add(child);
+        }
+    }
+}
---
+++ LangEditor/TreeEditor.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace LangEditor {
+
+    /// <summary>
+    /// TreeEditor.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TreeEditor : UserControl {
+        public ObservableCollection<LangData> TreeRoot { get; set; }
+        public TreeEditor() {
+            InitializeComponent();
+            text.Text = "test Text.";
+            TreeRoot = new ObservableCollection<LangData>();
+            var item1 = new LangData() { Text = "Item1", IsExpanded = true };
+            var item11 = new LangData() { Text = "Item1-1", IsExpanded = true };
+            var item12 = new LangData() { Text = "Item1-2", IsExpanded = true };
+            var item2 = new LangData() { Text = "Item2", IsExpanded = false };
+            var item21 = new LangData() { Text = "Item2-1", IsExpanded = true };
+            TreeRoot.Add(item1);
+            TreeRoot.Add(item2);
+            item1.Add(item11);
+            item1.Add(item12);
+            item2.Add(item21);
+
+            DataContext = this;
+        }
+    }
+}
